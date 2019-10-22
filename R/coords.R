173a0b6235c94d6af015e9e12ea3818cdb3da29f
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-10-22 16:09:24 +0300

    Working with updated API

--- R/coords.R
+++ R/coords.R
@@ -1,45 +1,38 @@
 CoordSci <-
     ggproto("CoordSci", CoordCartesian,
         # https://github.com/tidyverse/ggplot2/blob/115c3960d0fd068f1ca4cfe4650c0e0474aabba5/R/coord-cartesian-.r#L99
-        setup_panel_params = function(self, scale_x, scale_y, params = list()) {
-            append(ggproto_parent(CoordCartesian, self)$setup_panel_params(scale_x, scale_y, params),
-                   list(ticks_minor_size_f = self$ticks_minor_size_f))
-            #c(view_scales_from_scale(scale_x, self$limits$x, self$expand),
-              #view_scales_from_scale(scale_y, self$limits$y, self$expand),
-              #list(ticks_minor_size_f = self$ticks_minor_size_f))
-        } ,
-        # https://github.com/tidyverse/ggplot2/blob/23e324197e0a5ddd764588d42838b0d96da9b68d/R/coord-cartesian-.r#L116
-        #render_axis_h = function(panel_params, theme) {
-            #arrange <- panel_params$x.arrange %||% c("secondary", "primary")
-            #arrange_scale_keys <- c(primary = "x", secondary = "x.sec")[arrange]
-            #arrange_scales <- panel_params[arrange_scale_keys]
+        #setup_panel_params = function(self, scale_x, scale_y, params = list()) {
+            #res <- append(ggproto_parent(CoordCartesian, self)$setup_panel_params(scale_x, scale_y, params),
+                   #list(ticks_minor_size_f = self$ticks_minor_size_f))
 
-            #ticks_minor_size_f <- panel_params$ticks_minor_size_f
-            
-            #list(
-                 #top = draw_view_scale_axis(arrange_scales[[1]], "top", theme, ticks_minor_size_f),
-                 #bottom = draw_view_scale_axis(arrange_scales[[2]], "bottom", theme, ticks_minor_size_f))
-        #},
+            #print(res)
+            ##c(view_scales_from_scale(scale_x, self$limits$x, self$expand),
+              ##view_scales_from_scale(scale_y, self$limits$y, self$expand),
+              ##list(ticks_minor_size_f = self$ticks_minor_size_f))
+        #} ,
+        # https://github.com/tidyverse/ggplot2/blob/fa000f786cb0b641600b6de68ae0f96e2ffc5e75/R/coord-cartesian-.r#L138
+        train_panel_guides = function(self, panel_params, layers, default_mapping, params = list()) {
+            aesthetics <- c("x", "y", "x.sec", "y.sec")
+            names(aesthetics) <- aesthetics
+
+            panel_params$guides <- lapply(aesthetics, function(aesthetic) {
+                axis <- substr(aesthetic, 1, 1)
+                guide <- panel_params$guides[[aesthetic]]
+                ## Using modified training method
+                guide <- guide_train(guide, panel_params[[aesthetic]])
+                guide <- ggplot2:::guide_transform(guide, self, panel_params)
+                guide <- ggplot2:::guide_geom(guide, layers, default_mapping)
+                guide
+            })
+
+            panel_params
+        },
         # https://github.com/tidyverse/ggplot2/blob/115c3960d0fd068f1ca4cfe4650c0e0474aabba5/R/coord-cartesian-.r#L185
         render_axis_h = function(panel_params, theme) {
             list(
               top = panel_guides_grob(panel_params$guides, position = "top", theme = theme),
-              bottom = panel_guides_grob(panel_params$guides, position = "bottom", theme = theme)
-            )
-        } #,
-
-        # https://github.com/tidyverse/ggplot2/blob/23e324197e0a5ddd764588d42838b0d96da9b68d/R/coord-cartesian-.r#L127
-        #render_axis_v = function(panel_params, theme) {
-            #arrange <- panel_params$y.arrange %||% c("primary", "secondary")
-            #arrange_scale_keys <- c(primary = "y", secondary = "y.sec")[arrange]
-            #arrange_scales <- panel_params[arrange_scale_keys]
-
-            #ticks_minor_size_f <- panel_params$ticks_minor_size_f
-
-            #list(
-                #left = draw_view_scale_axis(arrange_scales[[1]], "left", theme, ticks_minor_size_f),
-                #right = draw_view_scale_axis(arrange_scales[[2]], "right", theme, ticks_minor_size_f))
-        #}
+              bottom = panel_guides_grob(panel_params$guides, position = "bottom", theme = theme))
+        }
     )
 
 coord_sci <- function(
@@ -51,12 +44,3 @@ coord_sci <- function(
             expand = expand, default = default, clip = clip,
             ticks_minor_size_f = ticks.minor.size.f)
 }
-
-
-ggplot(mtcars, aes(hp, mpg)) +
-    theme_scientific() +
-    scale_x_continuous() +
-    coord_sci() +
-    geom_point() -> plt
-
-print(plt)
