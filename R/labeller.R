41c287abce9cb120fb55f6f8e8d310c9097be1c8
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-10-28 12:22:04 +0200

    Cutom labeller

--- R/labeller.R
+++ R/labeller.R
@@ -1,4 +1,5 @@
-sample_labeller <- `class<-`(function(labels) {
+debug_labeller <- `class<-`(function(labels) {
+    print(labels)
     col_labs <- purrr::pmap(labels$cols, paste, sep = "; ")
     row_labs <- purrr::pmap(labels$rows, paste, sep = "; ")
 
@@ -6,4 +7,16 @@ sample_labeller <- `class<-`(function(labels) {
          right = paste0(row_labs, "-right"),
          top = paste0(col_labs, "-top"),
          bottom = paste0(col_labs, "-bottom"))
-}, "Labeller")
+}, "labeller")
+
+
+label_f <- function(.f_left = NULL, .f_right = NULL, .f_top = NULL, .f_bottom = NULL) {
+    .f_left <- if (rlang::is_null(.f_left)) rlang::as_function(~NULL) else rlang::as_function(.f_left)
+    .f_right <- if (rlang::is_null(.f_right)) rlang::as_function(~NULL) else rlang::as_function(.f_right)
+    .f_top <- if (rlang::is_null(.f_top)) rlang::as_function(~NULL) else rlang::as_function(.f_top)
+    .f_bottom <- if (rlang::is_null(.f_bottom)) rlang::as_function(~NULL) else rlang::as_function(.f_bottom)
+
+    `class<-`(function(labels) {
+        list(left = .f_left(labels), right = .f_right(labels), top = .f_top(labels), bottom = .f_bottom(labels))
+    }, "labeller")
+}
