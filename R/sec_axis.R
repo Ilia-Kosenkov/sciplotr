7b45451598d03aab60b849e3baa9955bc43d5ae6
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-10-29 17:06:06 +0200

    Improved sec axis behaviour

--- R/sec_axis.R
+++ R/sec_axis.R
@@ -4,7 +4,7 @@ sec_axis_sci <- function(
     name = waiver(),
     breaks = waiver(),
     labels = waiver(),
-    breaks_trans = waiver()) {
+    breaks_trans = derive()) {
     # sec_axis() historically accpeted two-sided formula, so be permissive.
     if (length(axis_trans) > 2) axis_trans <- axis_trans[c(1, 3)]
 
@@ -42,20 +42,19 @@ AxisSecondarySci <- ggproto("AxisSecondarySci", AxisSecondary,
         if (self$empty()) return()
         if (!is.function(self$trans)) stop("transformation for secondary axes must be a function", call. = FALSE)
         if (ggplot2:::is.derived(self$name) && !ggplot2:::is.waive(scale$name)) self$name <- scale$name
+        ## TODO: Currently ignored
         if (ggplot2:::is.derived(self$breaks)) self$breaks <- scale$breaks
         if (ggplot2:::is.waive(self$breaks)) self$breaks <- scale$trans$breaks
         if (ggplot2:::is.derived(self$labels)) self$labels <- scale$labels
         if (ggplot2:::is.derived(self$guide)) self$guide <- scale$guide
-        if (ggplot2:::is.derived(self$temp_trans)) self$temp_trans <- identity_sci_trans()
+        if (ggplot2:::is.derived(self$temp_trans)) self$temp_trans <- scale$trans
         if (rlang::is_null(self$temp_trans)) self$temp_trans <- identity_sci_trans()
     },
-
     ### ------------------ ###
     break_info = function(self, range, scale) {
         if (self$empty()) return()
         # Test for monotonicity on unexpanded range
         self$mono_test(scale)
-
         # Get scale's original range before transformation
         ## Original, equally spaced range
         ## `scale$trans` is `identity_sci`, appears to be inherited
@@ -63,10 +62,13 @@ AxisSecondarySci <- ggproto("AxisSecondarySci", AxisSecondary,
         along_range <- seq(range[1], range[2], length.out = self$detail)
         old_range <- scale$trans$inverse(along_range)
 
-
         # Create mapping between primary and secondary range
         ## Applies transofrm to src range
         full_range <- self$transform_range(old_range)
+        mono_ids <- determine_mono_range(full_range)
+
+        old_range <- old_range[mono_ids]
+        full_range <- full_range[mono_ids]
 
         # Get break info for the secondary axis
         new_range <- range(full_range, na.rm = TRUE)
@@ -81,11 +83,11 @@ AxisSecondarySci <- ggproto("AxisSecondarySci", AxisSecondary,
             old_val_minor_trans <- rescale(range_info$minor, from = c(0, 1), to = range)
         }
         else {
-            trans <- log10_sci_trans()
-            temp_scale <- self$create_scale(trans$transform(new_range), trans)
+            trans <- self$temp_trans
+            temp_scale <- self$create_scale(trans$transform(new_range))
             ## Here the break info is obtained for the sec axis
 
-            range_info <- temp_scale$break_info()
+            range_info <- temp_scale$break_info() #%>% print
             range_info$major_source <- trans$inverse(range_info$major_source)
             range_info$minor_source <- trans$inverse(range_info$minor_source)
 
@@ -96,7 +98,7 @@ AxisSecondarySci <- ggproto("AxisSecondarySci", AxisSecondary,
                     ~ lin(.x, full_range[.y], vctrs::vec_c(0, 1)))
                 val <- purrr::map2_dbl(
                     offset, inds,
-                    ~ lin(.x, vctrs::vec_c(0, 1), old_range[.y]))
+                    ~ lin(.x, vctrs::vec_c(0, 1), old_range[.y])) 
                 scale$trans$transform(val)
             }
 
@@ -132,7 +134,7 @@ AxisSecondarySci <- ggproto("AxisSecondarySci", AxisSecondary,
         range_info$minor_source[] <- old_val_minor_trans
 
         names(range_info) <- paste0("sec.", names(range_info))
-        range_info %T>% print
+        range_info 
     },
 
     ### ------------------ ###
@@ -156,3 +158,24 @@ AxisSecondarySci <- ggproto("AxisSecondarySci", AxisSecondary,
     ## `make_title` is trivial and is inherited
     ## `empry` is trivial and is inherited
 )
+
+determine_mono_range <- function(range) {
+    signs <- sign(diff(range))
+    plus <- sum(signs %==% +1)
+    minus <- sum(signs %==% -1)
+    nas <- sum(is.na(signs) | is.nan(signs))
+    if (nas > plus &&
+        nas > minus)
+            rlang::abort("Invalid secondaary axis extended range", "sciplotr_invalid_operation")
+    if (plus >= minus) {
+        ids <- which(signs %==% +1)
+        ids <- vctrs::vec_c(ids, max(ids) + 1L)
+        return(ids)
+    }
+    else {
+        ids <- which(signs %==% -1)
+        ids <- vctrs::vec_c(ids, max(ids) + 1L)
+        return(ids)
+    }
+
+}
