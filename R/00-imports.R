cffc446dfc23edf497df0a45a6ffcad6e45a9df6
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-04-04 17:49:12 +0300

    Fixing import errors

--- R/00-imports.R
+++ R/00-imports.R
@@ -1,7 +1,7 @@
 #' @import                  rlang vctrs ggplot2 grid gtable
 #' @importFrom tidyr        replace_na
 #' @importFrom tibble       enframe
-#' @importFrom scales       censor trans_new rescale
+#' @importFrom scales       censor trans_new rescale rescale_max
 #' @importFrom stats        median
 #' @importFrom dplyr        %>% inner_join mutate n pull filter
 #' @importFrom purrr        imap map map_chr map2 map2_chr pluck detect_index
